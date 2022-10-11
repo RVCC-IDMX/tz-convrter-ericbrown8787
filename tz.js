bbb52261519aa33c87f7af98d50b2ae57dbed1cc
Eric Brown <eric.brown8787@gmail.com>
2022-10-10 23:21:39 -0400

    Add timezone conversion
    Application is now able to convert from default timezone to timezone
    specified in command line argument.

--- tz.js
+++ tz.js
@@ -2,6 +2,11 @@ const moment = require('moment-timezone');
 
 moment.tz.setDefault('America/New_York');
 
-const targetTimezone = 'Europe/Paris';
-
+let targetTimezone;
+if (process.argv.length !== 3) {
+  console.log('Usage: node < script - file > <timezone>');
+  process.exit(1);
+} else {
+  targetTimezone = process.argv.at(2);
+}
 console.log(`The time at ${targetTimezone} is ${moment().tz(targetTimezone).format()}`);
