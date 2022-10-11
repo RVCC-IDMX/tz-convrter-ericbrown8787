63e5a514a88a2bd292edf40165b456f958ab975f
Eric Brown <eric.brown8787@gmail.com>
2022-10-11 00:43:34 -0400

    Add error checking for invalid target
    App now exits with error 2 upon entry of invalid target time zone

--- tz.js
+++ tz.js
@@ -5,17 +5,24 @@ moment.tz.setDefault('America/New_York');
 
 const targetTimezone = yargs.argv._[0];
 const params = yargs.argv;
+const validTimezones = moment.tz.names();
 
-if (!yargs.argv._[0]) {
+if (!targetTimezone) {
   console.log('Usage: node < script - file > <timezone>');
   process.exit(1);
+} else if (validTimezones.includes(targetTimezone) === false) {
+  console.log('Please enter a valid time zone');
+  process.exit(2);
 } else {
   const time = moment().tz(targetTimezone);
   let formattedTime;
+
+  // Checking for format flag
   if (params.format) {
     formattedTime = time.format('dddd, MMMM Do YYYY, h:mm:ss a');
   } else {
     formattedTime = time.format();
   }
+
   console.log(`The time at ${targetTimezone} is ${formattedTime}`);
 }
